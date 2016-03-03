10aab6f803d36b149e3e0b4d40b6356b4d3abea6
James Freeman <freeman.james.h@gmail.com>
2016-03-02 23:25:08 -0700

    almost have basic functionality....

--- public/arena.js
+++ public/arena.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
-
+  var storeCard;
   var deckData;
   var deckArray = [];
+  var handArray = [];
 
   // $('button').on('click', function() {
 
 
-    deckShuffle(convertToArray(getDeckFromLocalStorage()))
+  deckShuffle(convertToArray(getDeckFromLocalStorage()))
 
-    // getDeckFromLocalStorage();
-    // convertToArray(deckData)
+  // getDeckFromLocalStorage();
+  // convertToArray(deckData)
 
   // });
 
 
   function convertToArray(deckObject) {
-    deckArray =[];
+    deckArray = [];
     for (var key in deckObject) {
       for (var i = 0; i < deckObject[key][0]; i++)
-      deckArray.push(deckObject[key][2])
+        deckArray.push([deckObject[key][2], deckObject[key][1]])
 
     }
     return (deckArray);
@@ -31,7 +32,7 @@ $(document).ready(function() {
     if (window.localStorage.deckData) {
       deckData = JSON.parse(window.localStorage.deckData);
     }
-    return(deckData)
+    return (deckData)
   }
 
   function deckShuffle(sourceArray) {
@@ -47,7 +48,60 @@ $(document).ready(function() {
 
   // deckShuffle(array)
 
+  $('#library').on('click', function() {
+    console.log('Clicked the library div');
+    handArray.push(deckArray[0]);
+    deckArray.shift();
+    console.log('This is your hand array: ' + handArray);
+    turnHandArrayIntoHand();
+  })
 
 
+
+
+  // $('#hand0').html('<img src="' + handArray[0][1].image_url + '">');
+  // console.log("first card's image_url: "+handArray[0][1].image_url);
+  // $('#hand1').html('<img src="' + handArray[1][1].image_url + '">');
+  // console.log("first card's image_url: "+handArray[0][1].image_url);
+  // $('#hand2').html('<img src="' + handArray[2][1].image_url + '">');
+  // console.log("first card's image_url: "+handArray[0][1].image_url);
+  // $('#hand3').html('<img src="' + handArray[3][1].image_url + '">');
+  // console.log("first card's image_url: "+handArray[0][1].image_url);
+  // $('#hand4').html('<img src="' + handArray[4][1].image_url + '">');
+  // console.log("first card's image_url: "+handArray[0][1].image_url);
+  // $('#hand5').html('<img src="' + handArray[5][1].image_url + '">');
+  // console.log("first card's image_url: "+handArray[0][1].image_url);
+  // $('#hand6').html('<img src="' + handArray[6][1].image_url + '">');
+  // console.log("first card's image_url: "+handArray[0][1].image_url);
+  // $('#hand7').html('<img src="' + handArray[7][1].image_url + '">');
+  // console.log("first card's image_url: "+handArray[0][1].image_url);
+  // $('#hand8').html('<img src="' + handArray[8][1].image_url + '">');
+  // console.log("first card's image_url: "+handArray[0][1].image_url);
+
+  $('.hand').on('click', function() {
+    var handPosition = $(this).attr('data-handPosition');
+    // console.log('this hand position is: ' + handPosition);
+    storeCard = handArray[handPosition];
+    handArray.splice(handPosition, 1);
+    console.log('Storing: ' + storeCard);
+    console.log(storeCard[1].image_url)
+
+  })
+
+  $('td').click(function() {
+
+    console.log('whoooo');
+    $(this).html('<img class="tableTopImage" src="' + storeCard[1].image_url + '">');
+    storeCard = [];
+    turnHandArrayIntoHand();
+  })
+
+
+  function turnHandArrayIntoHand() {
+    for (var i = 0; i < handArray.length; i++) {
+      ($('#hand' + '' + i + '')).html('<img src="' + handArray[i][1].image_url + '">')
+      console.log('whoooo')
+    }
+  }
 
 });
