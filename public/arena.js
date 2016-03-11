b0aae4f6b3fd331f72c3faeb0dbfb9c6cfe8374a
James Freeman <freeman.james.h@gmail.com>
2016-03-11 13:20:44 -0700

    cards now switch

--- public/arena.js
+++ public/arena.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   var storeCard;
+  var switchCard;
+  var lastPos;
   var deckData;
   var deckArray = [];
   var handArray = [];
@@ -8,16 +10,18 @@ $(document).ready(function() {
 
   function tableGenerate() {
     for (var i = 0; i < 45; i++) {
-      console.log(tableObject['' + i + '']);
+      // console.log(tableObject['' + i + '']);
       if (tableObject['' + i + ''] === undefined) {
         $("div[data-index='" + i + "']").children().remove();
+      } else {
+        $("div[data-index='" + i + "']").html('<img class="tableTopImage" src="' + tableObject['' + i + ''][1].image_url + '">')
       }
     }
   }
 
   tableGenerate();
 
-  console.log(tableObject[1])
+  // console.log(tableObject[1])
 
   deckShuffle(convertToArray(getDeckFromLocalStorage()))
 
@@ -68,27 +72,58 @@ $(document).ready(function() {
     handArray.splice(handPosition, 1);
   })
 
+  // $('.cardSpace').click(function() {
+  //   var cardSpaceIndex = $(this).attr('data-index');
+  //   if (storeCard.length != 0) {
+  //     if ($(this).html().length == 0) {
+  //       tableObject['' + cardSpaceIndex + ''] = storeCard;
+  //       storeCard = [];
+  //       turnHandArrayIntoHand();
+  //       tableGenerate();
+  //     } else {
+  //       switchCard = tableObject['' + cardSpaceIndex + ''];
+  //       tableObject['' + cardSpaceIndex + ''] = storeCard;
+  //       storeCard = [];
+  //
+  //     }
+  //   } else if (tableObject['' + cardSpaceIndex + ''] != undefined) {
+  //     // tableGenerate();
+  //     storeCard = tableObject['' + cardSpaceIndex + ''];
+  //     delete(tableObject['' + cardSpaceIndex + ''])
+  //   }
+  //   console.log(tableObject);
+  // })
+
   $('.cardSpace').click(function() {
     var cardSpaceIndex = $(this).attr('data-index');
-    if (storeCard.length != 0) {
-      tableObject['' + cardSpaceIndex + ''] = storeCard;
-      $(this).html('<img class="tableTopImage" src="' + tableObject[cardSpaceIndex][1].image_url + '">');
+    console.log(storeCard)
+    console.log(cardSpaceIndex);
+    if (storeCard.length != 0) { // if you have a card stored //
+      if ($(this).html().length != 0) { // and you're clicking on an occupied space //
+        switchCard = tableObject['' + cardSpaceIndex + '']; // I want you save the card of the place you're clicking
+        tableObject['' + lastPos + ''] = switchCard; // and I want you to put it where the last card you clicked
+      };
+      tableObject['' + cardSpaceIndex + ''] = storeCard; // then I want you to make the occupied space the stored card //
       storeCard = [];
-      turnHandArrayIntoHand();
+      switchCard = [];
       tableGenerate();
-    }
-    else if (tableObject['' + cardSpaceIndex + ''] != undefined) {
-      tableGenerate();
-      storeCard = tableObject['' + cardSpaceIndex + ''];
+    } else {
+      lastPos = cardSpaceIndex;
+      if (tableObject['' + cardSpaceIndex + ''] != undefined) {
+
+
+        storeCard = tableObject['' + cardSpaceIndex + ''];
+      };
+      console.log(storeCard);
       delete(tableObject['' + cardSpaceIndex + ''])
     }
-    console.log(tableObject);
+    turnHandArrayIntoHand();
   })
 
 
   function turnHandArrayIntoHand() {
     $('.handCard').children().remove();
-    console.log(handArray.length)
+    // console.log(handArray.length)
     for (var i = 0; i < handArray.length; i++) {
       ($('#' + '' + i + '')).html('<img src="' + handArray[i][1].image_url + '">')
     }
